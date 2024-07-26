59027db83cd214081f553a5dfcc3d331d3536dce
Megan Lucas <1017630@student.sae.edu.com>
2024-07-27 05:29:29 +0800

    Created Button Prefab, Text, UI script and base scene

---
+++ Assets/Scripts/UiText.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; //adds textmesh pro engine
+
+public class UiText : MonoBehaviour
+{
+    public TextMeshProUGUI textBubble;
+    public TextMeshProUGUI nameBubble;
+    public TextMeshProUGUI hobbyBubble;
+    public TextMeshProUGUI buttonAText;
+    public TextMeshProUGUI buttonBText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
